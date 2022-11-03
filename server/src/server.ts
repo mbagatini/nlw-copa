23c55ca7dc6aabecb8300bed739426b115b96d27
Morgana <morganabagatini@gmail.com>
2022-11-03 14:52:17 -0300

    Count routes: return json

--- server/src/server.ts
+++ server/src/server.ts
@@ -48,17 +48,17 @@ async function bootstrap() {
 
 	fastify.get('/pools/count', async () => {
 		const count = await prisma.pool.count();
-		return count;
+		return { count };
 	})
 
 	fastify.get('/users/count', async () => {
 		const count = await prisma.user.count();
-		return count;
+		return { count };
 	})
 
 	fastify.get('/guesses/count', async () => {
 		const count = await prisma.guess.count();
-		return count;
+		return { count };
 	})
 
 	await fastify.listen({ port: 3333, host: '0.0.0.0' });
