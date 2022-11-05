153b15f6a267d06b7b9a1517960f3fef467523a5
Morgana <morganabagatini@gmail.com>
2022-11-05 09:53:00 -0300

    Button component

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -2,27 +2,30 @@ import { Button as BaseButton, IButtonProps, Text } from "native-base";
 import { ColorType } from "native-base/lib/typescript/components/types";
 
 interface ButtonProps extends IButtonProps {
-	children: React.ReactNode;
+	children: string;
 	color?: ColorType;
+	titleColor?: ColorType;
 }
 
-export function Button({ children, color = "yellow.500", ...rest }: ButtonProps) {
+export function Button({ children, color = "yellow.500", titleColor = "black", ...rest }: ButtonProps) {
 	const [name, tone] = color.toString().split('.');
-	const darkerColor = name + "." + (tone + 100);
+	const darkerTone = Number(tone) + (Number(tone) < 900 ? 100 : -100);
+	const darkerColor = name + "." + darkerTone;
 
 	return (
-		<BaseButton w="full"
-			h={14}
+		<BaseButton
+			w="full"
+			padding={5}
 			rounded="sm"
 			fontSize="md"
 			textTransform="uppercase"
-			color={color}
+			bg={color}
 			_pressed={{
 				bg: darkerColor
 			}}
 			{...rest}
 		>
-			<Text fontSize="sm" fontFamily="heading" color="white">{children}</Text>
+			<Text fontSize="sm" fontFamily="heading" color={titleColor}>{children}</Text>
 		</BaseButton>
 	)
 }
