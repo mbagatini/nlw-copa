4cf7c5753a76303204339ab58ebb3773cc6df84b
Morgana <morganabagatini@gmail.com>
2022-11-05 09:51:51 -0300

    Adding support to SVG

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,4 @@
-import { getDefaultConfig } from "expo/metro-config";
+const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (() => {
 	const config = getDefaultConfig(__dirname);
