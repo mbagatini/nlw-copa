3775517984fb2b238ca66734f717000967bee498
Morgana <morganabagatini@gmail.com>
2022-11-06 01:56:04 -0300

    Navigate to find route

--- mobile/src/pages/Polls.tsx
+++ mobile/src/pages/Polls.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Icon, VStack } from "native-base";
 import { Octicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 
 export function Polls() {
+	const { navigate } = useNavigation();
 
 	return (
 		<VStack flex={1} bgColor="gray.900" p={7}>
@@ -14,6 +16,7 @@ export function Polls() {
 			<VStack mt={6} mx={5} pb={4} mb={4} borderBottomWidth={1} borderBottomColor="gray.600">
 				<Button
 					leftIcon={<Icon as={Octicons} color="black" size="md" />}
+					onPress={() => navigate('find')}
 				>
 					BUSCAR BOLÃO POR CÓDIGO
 				</Button>
