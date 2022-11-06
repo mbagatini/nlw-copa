7037fc8174867e55b0dcdff19b6e62a0d7074992
Morgana <morganabagatini@gmail.com>
2022-11-05 21:27:36 -0300

    Faker to seed database

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,26 +1,8 @@
+import { faker } from "@faker-js/faker";
+
 import { prisma } from "../src/database/prisma";
 
 async function main() {
-	const user = await prisma.user.create({
-		data: {
-			name: 'John Doe',
-			email: 'john@doe.com',
-			avatarUrl: 'https://avatars.dicebear.com/api/micah/avavavavav.svg',
-			googleId: 'myfakeid'
-		}
-	});
-
-	const poll = await prisma.poll.create({
-		data: {
-			title: 'The great winner',
-			code: 'WEHEX4',
-			ownerId: user.id,
-			participants: {
-				create: { userId: user.id }
-			}
-		}
-	});
-
 	// create game without guesses
 	await prisma.game.create({
 		data: {
@@ -30,29 +12,60 @@ async function main() {
 		}
 	});
 
-	// create game with a guess
-	await prisma.game.create({
-		data: {
-			date: '2022-12-12T12:00:00.737Z',
-			teamACountryCode: 'BR',
-			teamBCountryCode: 'MX',
+	for (let index = 0; index <= 15; index++) {
+		const firstName = faker.name.firstName();
+		const lastName = faker.name.lastName();
 
-			guesses: {
-				create: {
-					teamAPoints: 3,
-					teamBPoints: 1,
-					participant: {
-						connect: {
-							userId_pollId: {
-								pollId: poll.id,
-								userId: user.id
+		const userFake = {
+			name: firstName + " " + lastName,
+			email: faker.internet.email(firstName, lastName),
+			avatarUrl: `https://avatars.dicebear.com/api/micah/${faker.random.word()}.svg`,
+			googleId: faker.datatype.uuid()
+		};
+
+		const user = await prisma.user.create({
+			data: userFake
+		});
+
+		const poll = await prisma.poll.create({
+			data: {
+				title: faker.music.songName(),
+				code: faker.datatype.uuid().substring(0, 7).toUpperCase(),
+				ownerId: user.id,
+				participants: {
+					create: { userId: user.id }
+				}
+			}
+		});
+
+		// create game with a guess
+		await prisma.game.create({
+			data: {
+				date: '2022-12-12T12:00:00.737Z',
+				teamACountryCode: 'BR',
+				teamBCountryCode: 'MX',
+
+				guesses: {
+					create: {
+						teamAPoints: 3,
+						teamBPoints: 1,
+						participant: {
+							connect: {
+								userId_pollId: {
+									pollId: poll.id,
+									userId: user.id
+								}
 							}
 						}
 					}
 				}
 			}
-		}
-	});
+		});
+	}
+
+
+
+
 }
 
 main();
